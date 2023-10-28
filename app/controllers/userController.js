9725b18f381e843801353023da6e4a4a3fd88e80
Shuli-Wang <julieswang00@gmail.com>
2023-10-28 15:29:29 -0400

    Add something about fetching contactus data

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -35,4 +35,23 @@ exports.getAllPatients = async (req, res) => {
 };
 
 
+exports.getContactUs = async (req, res) => {
+
+  console.log("contact")
+
+  try {
+    const contact = await db.sequelize.query(
+      "SELECT * FROM nkw2tiuvgv6ufu1z.contact_us",
+      { type: QueryTypes.SELECT }
+    );
+    res.json(contact);
+  } catch (error) {
+    console.error("Error fetching contact:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+
+
 
